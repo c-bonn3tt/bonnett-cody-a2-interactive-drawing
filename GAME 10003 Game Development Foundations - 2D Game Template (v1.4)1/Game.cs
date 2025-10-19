f1003cbfa57f2cadf3980721d4d63da8eb30b7f3
c-bonn3tt <cody.bonnett@mohawkcollege.ca>
2025-10-19 18:29:22 -0400

    constant issues

---
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
@@ -0,0 +1,41 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Drawing");
+            Window.SetSize(800, 600);
+            Window.TargetFPS = 60;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.Black);
+           
+            //draw sun
+
+            Color orange = new Color(255, 165, 0);
+            Draw.FillColor = orange;
+            Draw.Circle(Input.GetMouseX(), Input.GetMouseY(), 50);
+        }
+    }
+
+}
