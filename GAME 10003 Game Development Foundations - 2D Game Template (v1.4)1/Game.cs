c3420c92f8d697ae41fe4eddf9d8c6779bd17d28
c-bonn3tt <cody.bonnett@mohawkcollege.ca>
2025-10-19 19:36:12 -0400

    Created planets

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
@@ -34,7 +34,30 @@ namespace MohawkGame2D
 
             Color orange = new Color(255, 165, 0);
             Draw.FillColor = orange;
-            Draw.Circle(Input.GetMouseX(), Input.GetMouseY(), 50);
+            Draw.Ellipse(-300, 300, 900, 900);
+
+            // draw mercury
+            Draw.FillColor = Color.Gray;
+            Draw.Ellipse(200, 300, 30, 30);
+
+            //draw venus
+            Color paleOrange = new Color(255, 179, 138);
+            Draw.FillColor = paleOrange;
+            Draw.Ellipse(300, 300, 70, 70);
+
+            //draw earth
+            Draw.FillColor = Color.Blue;
+            Draw.Ellipse(400, 300, 80, 80);
+
+            //draw mars 
+            Color redOrange = new Color(255, 116, 0);
+            Draw.FillColor = redOrange;
+            Draw.Ellipse(500, 300, 40, 40);
+
+            //draw jupiter
+            Color paleYellow = new Color(255, 227, 192);
+            Draw.FillColor = paleYellow;
+            Draw.Ellipse(700, 300, 300, 300);
         }
     }
 
